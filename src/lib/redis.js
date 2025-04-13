13af8f380d4f81a1a8a5093a6d1cead9838f708f
umrsain <umrsain@gmail.com>
2025-04-13 13:43:32 +0400

    Update redis.js

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis'
 
 export const redis = new Redis({
-  url: 'https://ultimate-cicada-26432.upstash.io',
+  url: 'https://grateful-dogfish-19683.upstash.io',
   token: process.env.REDIS_KEY,
 })
